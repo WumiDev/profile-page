57ae1cabc25361b8ba9aa10159cf46ef14c87831
WumiDev <wumi2021@gmail.com>
2022-10-29 20:02:04 +0100

    Create Main.js

---
+++ src/components/Main.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export default function 
+Main() {
+  return (
+    <header>
+        <img src="" id="profile_img" alt=""/>
+        <h3>Wumi Ola</h3>
+    </header>
+  )
+}
