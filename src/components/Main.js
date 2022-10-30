0bf39d2e16ba68757b58c63ce312603bbb7f09cd
WumiDev <wumi2021@gmail.com>
2022-10-30 15:54:30 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import profileImg from "../images/wumi.jpg"
+import slackIcon from "../images/slack-icon.png"
+import githubIcon from "../images/github-icon.png"
 
 export default function 
 Main() {
@@ -8,17 +10,18 @@ Main() {
         <header>
             <img src={profileImg} id="profile_img" alt="wumi"/>
             <h3>Wumi Ola</h3>
+            <p id="slack" className="invisible">WumiDev</p>
         </header>
+
         <main>
             <a href="https://twitter.com/WumiDev" id="twitter"><button>Twitter Link</button></a>
-            <p id="slack">WumiDev</p>
-            <button><a href="https://training.zuri.team/" id="btn_zuri">Zuri Team</a></button>
-            <button><a href="https://books.zuri.team" id="books">Zuri Books</a></button>
-            <button><a href="https://books.zuri.team/python-for-beginners?ref_id=wumidev" id="book_python">Python Books</a></button>
-            <button><a href="https://background.zuri.team" id="pitch">Background Check for Coders</a></button>
-            <button><a href="https://books.zuri.team/design-rules" id="book_design">Design Books</a></button>
-            <a href="https://styling.zuri.team/design-rules" id="book_design"><button>Styling Books</button></a>
-
+            <a href="https://training.zuri.team/" id="btn_zuri"><button>Zuri Team</button></a>
+            <a href="https://books.zuri.team" id="books"><button>Zuri Books</button></a>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=wumidev" id="book_python"><button>Python Books</button></a>
+            <a href="https://background.zuri.team" id="pitch"><button>Background Check for Coders</button></a>
+            <a href="https://books.zuri.team/design-rules" id="book_design"><button>Design Books</button></a>
+            <a href="#"><img src={slackIcon} id="slack" alt="slack icon"/></a>
+            <a href="https://github.com/WumiDev"><img src={githubIcon} id="github" alt="github icon"/></a>
         </main>
     </>
   )
