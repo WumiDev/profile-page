88b9a78b149d923f6a8ef9f534e92e54de382e78
WumiDev <wumi2021@gmail.com>
2022-11-01 14:06:49 +0100

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -15,7 +15,7 @@ Main() {
         </header>
 
         <main className="main-container">
-            <a href="https://twitter.com/WumiDev" id="btn_twitter">Twitter Link</a>
+            <a href="https://twitter.com/WumiDev" id="btn__twitter">Twitter Link</a>
             <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
             <a href="https://books.zuri.team" id="books">Zuri Books</a>
             <a href="https://books.zuri.team/python-for-beginners?ref_id=wumidev" id="book__python">Python Books</a>
