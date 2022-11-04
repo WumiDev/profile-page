c1921b829cc0cdeb30b3d066ae8ba167e08b8ffc
WumiDev <wumi2021@gmail.com>
2022-11-04 14:34:18 +0100

    feat: add Contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+// import React from 'react'
+import ContactForm from "../components/ContactForm";
+import Footer from "../components/Footer"
+
+export default function Home() {
+  return (
+    <section className="section-container">
+        <ContactForm />
+        <Footer /> 
+    </section>
+  )
+}
