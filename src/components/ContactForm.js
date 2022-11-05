5128b777a55564a060ebec0b37bcef15c4d139f5
WumiDev <wumi2021@gmail.com>
2022-11-05 09:42:10 +0100

    fix: add redirects to rectify netlify 404 error

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -9,26 +9,26 @@ ContactForm() {
           <p>Hi there, contact me to ask me about anything you have in mind.</p>
             <div className="form-control">
                 <label for="first_name">First name</label>
-                <input type="text" id="first_name" placeholder="Enter your first name" />
+                <input type="text" id="first_name" placeholder="Enter your first name" required />
             </div>
 
             <div className="form-control">
                 <label for="last_name">Last name</label>
-                <input type="text" id="last_name" placeholder="Enter your last name" />
+                <input type="text" id="last_name" placeholder="Enter your last name" required />
             </div>
 
             <div className="form-control">
                 <label for="email">Email</label>
-                <input type="email" id="email" placeholder="yourname@email.com" />
+                <input type="email" id="email" placeholder="yourname@email.com" required/>
             </div>
 
             <div className="form-control">
                 <label for="message">Message</label>
-                <textarea id="message" name="message" placeholder="Send me a message and I'll reply you as soon as possible..." rows="4" cols="50"></textarea>
+                <textarea id="message" name="message" placeholder="Send me a message and I'll reply you as soon as possible..." rows="4" cols="50" required></textarea>
             </div>
 
             <div className="form-control">
-                <input type="checkbox" id="agreement" name="agreement" value="" />
+                <input type="checkbox" id="agreement" name="agreement" value="" required/>
                 <label for="agreement">You agree to providing your data to WumiDev who may contact you.</label>
             </div>
 
