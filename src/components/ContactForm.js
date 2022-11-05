dbb56fd3d46de3aa8f98a7de94c8f21a80538b08
WumiDev <wumi2021@gmail.com>
2022-11-05 19:27:52 +0100

    style: add styling to ContactForm.js

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,18 +3,20 @@ import "../style.css"
 export default function 
 ContactForm() {
   return (
-    <section className="section-container">
-        <form id="form" className="form">
-            <h2>Contact Me</h2>
-          <p>Hi there, contact me to ask me about anything you have in mind.</p>
-            <div className="form-control">
-                <label for="first_name">First name</label>
-                <input type="text" id="first_name" placeholder="Enter your first name" required />
-            </div>
+    // <section className="section-container">
+        <form id="form" className="form-container">
+            <h1>Contact Me</h1>
+          <p className="supporting-text">Hi there, contact me to ask me about anything you have in mind.</p>
+            <div className="input-row">
+                <div className="form-control">
+                    <label for="first_name">First name</label>
+                    <input type="text" id="first_name" placeholder="Enter your first name" required />
+                </div>
 
-            <div className="form-control">
-                <label for="last_name">Last name</label>
-                <input type="text" id="last_name" placeholder="Enter your last name" required />
+                <div className="form-control">
+                    <label for="last_name">Last name</label>
+                    <input type="text" id="last_name" placeholder="Enter your last name" required />
+                </div>
             </div>
 
             <div className="form-control">
@@ -24,16 +26,16 @@ ContactForm() {
 
             <div className="form-control">
                 <label for="message">Message</label>
-                <textarea id="message" name="message" placeholder="Send me a message and I'll reply you as soon as possible..." rows="4" cols="50" required></textarea>
+                <textarea id="message" name="message" placeholder=" Send me a message and I'll reply you as soon as possible..." rows="4" cols="50" required></textarea>
             </div>
 
-            <div className="form-control">
+            <div className="form-control-checkbox">
                 <input type="checkbox" id="agreement" name="agreement" value="" required/>
-                <label for="agreement">You agree to providing your data to WumiDev who may contact you.</label>
+                <label for="agreement"> You agree to providing your data to WumiDev who may contact you.</label>
             </div>
 
-            <button>Submit</button>
+            <button id="btn__submit">Submit</button>
         </form>
-    </section>
+    // </section>
   )
 }
